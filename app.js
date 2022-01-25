4f57393e3eec3e219f08f06172f345bc5540745c
naluwn <street.baller06@gmail.com>
2022-01-25 17:38:25 +0800

    feat: add dark mode event

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-// const input = document.querySelectorAll('#action__input_1')
 const tbody = document.querySelector('.table__body')
-
-
-// 只能操作第一個input和menu
-// input.addEventListener('click', e => {
-//   console.log(input)
-//   const menu = document.querySelectorAll('#action__menu_1')
-//   menu.classList.toggle('hidden')
-//   // const target = e.target
-//   // if(target.matches('#action__input_1')){
-//   //   console.log(target)
-//   //   const menu = document.querySelector(`${target}~ #action__menu_1`)
-//   //   menu.classList.toggle('hidden')
-//   // }
-// })
+const darkModeToggle = document.querySelector('#dark__mode__toggle')
 
 // 可以操作所有input和menu
 tbody.addEventListener('click', e => {
@@ -22,5 +8,15 @@ tbody.addEventListener('click', e => {
     // nextElementSibling 抓取同層下一個element
     const menu = target.nextElementSibling
     menu.classList.toggle('hidden')
+  }
+})
+
+// 因為目標是checkbox不是button，所以事件要用change，不是click
+darkModeToggle.addEventListener('change', e => {
+  const target = e.target
+  if(target.checked){
+    document.documentElement.setAttribute('data-theme', 'dark')
+  }else{
+    document.documentElement.setAttribute('data-theme', 'light')
   }
 })
